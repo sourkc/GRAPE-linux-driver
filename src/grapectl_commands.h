8addb3969806c9f0844d733a93aee27b2d1ca3cf
Sour KC <imsourkc@gmail.com>
2026-08-13 17:08:21 +0300

    Add rect command to grapectl

--- src/grapectl_commands.h
+++ src/grapectl_commands.h
@@ -1,30 +1,35 @@
 #pragma once
+#include "grapectl_surface.h"
 
 static void grapectl_usage(const char *program)
 {
-    fprintf(stderr, "Usage: %s hello | info\n", program);
+    fprintf(stderr, "Usage: %s hello | info | rect <x> <y> <width> <height> <RRGGBB|RRGGBBAA>\n", program);
 }
 
 static int grapectl_dispatch(libusb_device_handle *device, int argc, char **argv)
 {
-    uint8_t payload[64];
-    uint32_t size = 0;
+    if (!strcmp(argv[1], "rect")) return grapectl_rect(device, argc, argv);
     if (argc != 2) {
         grapectl_usage(argv[0]);
         return 2;
     }
+
     uint8_t opcode = !strcmp(argv[1], "hello") ? GFXLINK_OP_HELLO :
                      !strcmp(argv[1], "info") ? GFXLINK_OP_GET_INFO : 0;
     if (!opcode) {
         grapectl_usage(argv[0]);
         return 2;
     }
+
+    uint8_t payload[64];
+    uint32_t size = 0;
     int ret = grapectl_request(device, opcode, NULL, 0, payload, sizeof(payload), &size);
     if (ret != 0) {
         fprintf(stderr, "USB error: %s\n", libusb_error_name(ret));
         return 1;
     }
     if (size < sizeof(gfxlink_status_response_t) || grapectl_status(payload) != GFXLINK_STATUS_OK) return 1;
+
     if (opcode == GFXLINK_OP_HELLO && size == sizeof(gfxlink_hello_response_t)) {
         gfxlink_hello_response_t response;
         memcpy(&response, payload, sizeof(response));
